e5dab5d0e174b6b918b47c2706d98f32369dd67d
Zsombor Horvath <horvathzsombor2005@gmail.com>
2022-09-30 22:50:13 +0200

    parsed data

--- app.py
+++ app.py
@@ -4,13 +4,33 @@ import pandas
 
 app = Flask(__name__)
 
+
 @app.route('/testing')
 def testing():
-    df = pandas.read_csv('first.csv')
-    return f"<p>hello</p>"
+    fileNames = ["Group_340", "Group_342",
+                 "Group_343", "Group_349", "Group_416"]
+
+    fileValues = []
+    for fileName in fileNames:
+        df = pandas.read_csv(f'csvfiles/test1/{fileName}.csv')
+        keys = []
+        longvalues = []
+
+        for name in df.columns:
+            keys.append(name)
+
+        for index, row in df.iterrows():
+            value = []
+            for key in keys:
+                value.append(row[key])
+            longvalues.append(value)
+
+        fileValues.append(longvalues)
+
+    # return filevalues in html
+    return str(fileValues)
 
 
 @app.route("/")
 def response():
     return f"<p>{ lib.test() }</p>"
-
