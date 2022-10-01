c5e91f4ae8c5f3de22678740c2cba605fb6bf61f
Zsombi <horvathzsombor2005@gmail.com>
2022-10-01 12:39:36 +0200

    python cpp

--- lib/python.cpp
+++ lib/python.cpp
@@ -14,7 +14,6 @@ std::string test()
 
 std::string testing(std::vector<std::vector<std::vector<float>>> values)
 {
-
     float first = 0.01;
     float b = ceil(values[4][values[4].size() - 1][0] * 100.0) / 100.0;
     World w;
@@ -51,15 +50,52 @@ std::string testing(std::vector<std::vector<std::vector<float>>> values)
             if (timestamp == int(f * 100))
             {
                 std::vector<MeasuredState> object_states;
-                for (int o = 32; o <= 46; o++)
+
+                for (int o = 0; o <= 14; o++)
                 {
-                    MeasuredState state;
-                    state.type = ObjectType((int)values[3][i][o]);
-                    for (int p = 0; p < 3; p++)
+                    if ((int)values[3][i][o + 32] != 0)
                     {
-                        state.error[p] = 0;
+                        MeasuredState state;
+                        state.type = ObjectType((int)values[3][i][o + 32]);
+                        for (int p = 0; p < 3; p++)
+                        {
+                            state.error[p] = INFINITE_ERROR;
+                        }
+
+                        state.position[0] = values[3][i][o + 2] / 128.0;
+                        state.position[1] = values[3][i][o + 17] / 128.0;
+                        state.position[2] = 0;
+                        state.velocity[0] = values[3][i][o + 47] / 256.0;
+                        state.velocity[1] = values[3][i][o + 62] / 256.0;
+                        state.velocity[2] = 0;
+                        state.acceleration[0] = 0;
+                        state.acceleration[1] = 0;
+                        state.acceleration[2] = 0;
+
+                        object_states.push_back(state);
                     }
-                    object_states.push_back(state);
+                }
+                for (int y = 0; y < 41; y += 4)
+                {
+                    for (int t = 0; t < 4; t++)
+                    {
+                        MeasuredState state;
+                        state.type = ObjectType::unknown;
+                        for (int p = 0; p < 3; p++)
+                        {
+                            state.error[p] = 0;
+                        }
+                        state.acceleration[0] = values[3][i][67 + t + y] / 2048.0;
+                        state.acceleration[1] = values[3][i][108 + t + y] / 2048.0;
+                        state.acceleration[2] = 0;
+                        state.position[0] = values[3][i][149 + t + y] / 128.0;
+                        state.position[1] = values[3][i][190 + t + y] / 128.0;
+                        state.position[2] = values[3][i][231 + t + y] / 128.0;
+                        state.velocity[0] = values[3][i][313 + t + y] / 256.0;
+                        state.velocity[1] = values[3][i][354 + t + y] / 256.0;
+                        state.velocity[2] = values[3][i][395 + t + y] / 256.0;
+                        object_states.push_back(state);
+                    }
                 }
                 tick.object_states = object_states;
             }
