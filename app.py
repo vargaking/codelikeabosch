ec4471e3838e2954c2ed3d83d13e1b1cab857e46
Daniel Gergely <vargaking2005@gmail.com>
2022-10-02 09:42:51 +0200

    routing

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from tracemalloc import Snapshot
 from flask import Flask, request
 from flask_cors import CORS
 import lib
@@ -8,16 +7,19 @@ import json
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/testing')
-def testing():
-    print("asd")
-    
+@app.route('/')
+def root():
+    return "The recordings should be placed into folder 'csvfiles'."
+
+@app.route('/view/<path:data>')
+def testing(data):
+
     fileNames = ["Group_340", "Group_342",
                  "Group_343", "Group_349", "Group_416"]
 
     fileValues = []
     for fileName in fileNames:
-        df = pandas.read_csv(f'csvfiles/test1/{fileName}.csv')
+        df = pandas.read_csv(f'csvfiles/{data}/{fileName}.csv')
         keys = []
         longvalues = []
 
@@ -32,20 +34,8 @@ def testing():
 
         fileValues.append(longvalues)
 
+    # simulating ticks and retrieving snapshots every 0.1 sec
+    # c++ backend
     snapshots = lib.testing(fileValues)
-    
-    
-    """# load ft_test.json into snapshots variable
-    with open('ft_test.json') as f:
-        snapshots = json.load(f)"""
-
-    return snapshots
-
-
-@app.route('/testing2')
-def testing2():
-    # load ft_test.json into snapshots variable
-    with open('ft_test.json') as f:
-        snapshots = json.load(f)
 
     return snapshots
